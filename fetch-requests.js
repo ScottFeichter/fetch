7a906bb7f7acf0325e948027524bbf1acf82236a
rcerney <ryanccerney@gmail.com>
2024-05-22 16:53:53 -0700

    phase 2

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,12 +6,33 @@ Paste your code for fetch requests here once you finish each phase.
 /* ============================== Phase 1 ============================== */
 
 // Your code here 
-
+fetch("http://localhost:5000/products", {
+    method: 'POST',
+    body: "name=Caribbean2+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
 
 
 /* ============================== Phase 2 ============================== */
 
 // Your code here 
+// fetch("http://localhost:5000/products", {
+//     method: 'POST',
+//     body: "name=Caribbean2+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded"
+//     }
+//   }).then(response => console.log(response.status, response.url, response.headers.get('Content-Type')))
+
+  fetch("http://localhost:5000/products", {
+    method: 'POST',
+    body: "name=Caribbean2+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  }).then(response => console.log(response.status, response.url, response.headers.get('Content-Type')))
 
 
 
